28a36a9b26404babe213e767a4f43266211ac84d
ivancho277 <rouskoi@live.com>
2019-04-24 07:42:21 -0700

    added route todos

---
+++ routes/api-routes.js
@@ -0,0 +1,12 @@
+TODO:
+//Post route to save article from reddit to db
+
+TODO:
+//get route to grab article from db and save to users articles
+
+TODO:
+//Delete route to get rid of article
+
+TODO:
+//get route for scraping
+
