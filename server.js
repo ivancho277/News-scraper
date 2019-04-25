95b2adecba7d259473ea72d350d491529e3fa7dc
ivancho277 <rouskoi@live.com>
2019-04-24 23:40:30 -0700

    curley braces mess everything up

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.get("/", (req, res) => {
       article: data
     }).catch(err => {
       res.json(err)
+    })
   })
 })
 
@@ -77,56 +78,58 @@ app.get("/all", (req, res) => {
 //TODO:
 //Delete route to get rid of article
 
-  app.delete("/", (req, res) => {
-    db.Article.deleteMany({}).then((data) => {
-      res.json()
-    }).catch(err => {
-      res.json(err)
+app.delete("/", (req, res) => {
+  db.Article.deleteMany({}).then((data) => {
+    res.json()
+  }).catch(err => {
+    res.json(err)
   })
 
-//TODO:
-//get route for scraping
-var results = [];
-app.get("/scrape", (req, res) => {
-  axios.get("https://old.reddit.com/r/javascript/").then(function (response) {
+  //TODO:
+  //get route for scraping
+  var results = [];
+  app.get("/scrape", (req, res) => {
+    axios.get("https://old.reddit.com/r/javascript/").then(function (response) {
 
-    // Load the Response into cheerio and save it to a variable
-    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-    var $ = cheerio.load(response.data);
+      // Load the Response into cheerio and save it to a variable
+      // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+      var $ = cheerio.load(response.data);
 
-    // An empty array to save the data that we'll scrape
+      // An empty array to save the data that we'll scrape
 
 
-    // With cheerio, find each p-tag with the "title" class
-    // (i: iterator. element: the current element)
-    $("p.title").each(function (i, element) {
-      // Save the text of the element in a "title" variable
-      var title = $(element).text();
-      // In the currently selected element, look at its child elements (i.e., its a-tags),
-      // then save the values for any "href" attributes that the child elements may have
-      var link = $(element).children().attr("href");
-      var result = {};
-      result.title = title;
-      result.link = link;
-      db.Article.create(result).then((dbArticle) => {
-        console.log(dbArticle);
-      }).catch(err => {
-        console.log(err);
-      })
+      // With cheerio, find each p-tag with the "title" class
+      // (i: iterator. element: the current element)
+      $("p.title").each(function (i, element) {
+        // Save the text of the element in a "title" variable
+        var title = $(element).text();
+        // In the currently selected element, look at its child elements (i.e., its a-tags),
+        // then save the values for any "href" attributes that the child elements may have
+        var link = $(element).children().attr("href");
+        var result = {};
+        result.title = title;
+        result.link = link;
+        db.Article.create(result).then((dbArticle) => {
+          console.log(dbArticle);
+        }).catch(err => {
+          console.log(err);
+        })
 
-      // Save these results in an object that we'll push into the results array we defined earlier
-      // results.push({
-      //   title: title,
-      //   link: link
-      // });
+        // Save these results in an object that we'll push into the results array we defined earlier
+        // results.push({
+        //   title: title,
+        //   link: link
+        // });
+      });
     });
+    //res.json(collections);
+    // Log the results once you've looped through each of the elements found with cheerio
+    //console.log(results);
+    setTimeout(() => {
+      res.redirect('/')
+    }, 300);
   });
   //res.json(collections);
-  // Log the results once you've looped through each of the elements found with cheerio
-  //console.log(results);
-  setTimeout(() => {res.redirect('/')}, 300);
-});
-//res.json(collections);
 
 
 
@@ -136,7 +139,9 @@ app.get("/scrape", (req, res) => {
 
 
 
-// Syncing our database and logging a message to the user upon success
-app.listen(PORT, function () {
-  console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-});
+  // Syncing our database and logging a message to the user upon success
+  app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
+
+})
